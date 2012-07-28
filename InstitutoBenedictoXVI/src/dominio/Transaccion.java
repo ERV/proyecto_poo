969c5ac6b701747eacc85f8fcf792bb717d2b767
Gian Carlo Morin <gian_carlo_morin@hotmail.com>
2012-07-28 11:13:13 -0500

    Envio Prueba Modificacion 1 Proyecto POO

--- InstitutoBenedictoXVI/src/dominio/Transaccion.java
+++ InstitutoBenedictoXVI/src/dominio/Transaccion.java
@@ -1,5 +1,19 @@
 package dominio;
+import org.joda.time.LocalDate;
 
 public class Transaccion {
-
+	public enum Estado {NUEVO, ANULADA, CANCELADA}	
+	
+	private String concepto;	
+	private int numero;
+	private LocalDate fechaEmision;
+	private String empresa;
+	private double subtotal;
+	private double igv;
+	private double total;
+	private String moneda;
+	private LocalDate fechaVencimiento;
+	private LocalDate fechaPago;
+	private dominio.Venta.Estado estado;
+	private String observaciones;	
 }
